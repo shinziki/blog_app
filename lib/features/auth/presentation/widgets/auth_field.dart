ab16ce48375d389aed8a4ab629085efc6bb24bf5
Sherwin Dave Osma <sherwinosma1@gmail.com>
2025-12-15 21:36:07 +0800

    AuthField functionality added for hintText

--- lib/features/auth/presentation/widgets/auth_field.dart
+++ lib/features/auth/presentation/widgets/auth_field.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 
 class AuthField extends StatelessWidget {
-  const AuthField({super.key});
+  final String hintText;
+  const AuthField({
+    super.key,
+    required this.hintText,
+  });
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       decoration: InputDecoration(
-        hintText: 
+        hintText: hintText,
       ),
     );
   }
