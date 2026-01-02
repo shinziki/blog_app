6f6bb53bc57b4169a56b63fa6941e682c6a0b5ff
shinziki <sherwinosma1@gmail.com>
2026-01-02 17:30:03 +0800

    Credential Input Padding Modification

--- lib/features/auth/presentation/pages/signup_page.dart
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -13,7 +13,7 @@ class _SignUpPageState extends State<SignUpPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(15.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
