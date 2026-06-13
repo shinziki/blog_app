a2de768b33bd0b653c49fc9fe604db4c0f2a425c
shinziki <sherwinosma1@gmail.com>
2026-06-13 09:40:41 +0800

    AuthRemoteDataSourceImpl implementation for Sign Up

--- lib/features/auth/data/datasources/auth_remote_data_source.dart
+++ lib/features/auth/data/datasources/auth_remote_data_source.dart
@@ -1,3 +1,5 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
 abstract interface class AuthRemoteDataSource {
   Future<String> signUpWithEmailPassword({
     required String name,
@@ -5,12 +7,28 @@ abstract interface class AuthRemoteDataSource {
     required String password,
   });
   Future<String> loginWithEmailPassword({
-    required String name,
     required String email,
     required String password,
   });
 }
 
 class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
-  
+  final SupabaseClient supabaseClient;
+  AuthRemoteDataSource
+  @override
+  Future<String> loginWithEmailPassword({
+    required String email,
+    required String password
+  }) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<String> signUpWithEmailPassword({
+    required String name,
+    required String email,
+    required String password
+   }) {
+    throw UnimplementedError();
+  }
 }
